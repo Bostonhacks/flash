e2fd0b013ed5f596382d5e66125a99002f2287b7
Emmanuel Amponsah <emampons@gmail.com>
2019-03-23 12:58:20 -0400

    added doc strings

--- flag_parser.py
+++ flag_parser.py
@@ -2,13 +2,20 @@ import argparse
 
 
 def parse_input():
+    """Parses flags from the command line
+    Returns:
+        str: returns the project directory  or the name of the directory 
+    """
     flag_parser = setup_parser()
     arguments = flag_parser.parse_args()
     project_dir = arguments.projectdir
-    print(arguments, project_dir)
     return project_dir[0]
 
 def setup_parser():
+    """Sets up flag parser
+    Returns:
+        parser: returns a flag parser
+    """
     flag_parser = argparse.ArgumentParser(description='Deployments in <2 minutes')
     flag_parser.add_argument('-d', nargs=1, dest='projectdir', help='deploying a dynamic project to remote location')
     flag_parser.add_argument('-s', nargs=1, dest='projectdir', help='serving a static project to a remote location')
